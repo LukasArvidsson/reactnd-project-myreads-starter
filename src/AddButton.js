6f69bde3c7b704afd3bb148c08fdeb0ea25b1419
Lukas <lukas.arvidsson@gmail.com>
2019-06-27 11:17:23 +0200

    Started to split up into components

---
+++ src/AddButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class AddButton extends React.Component {
+
+    return() {
+        render(
+            <div className="open-search">
+                <button onClick={() => ({})}>Add a book</button>
+            </div>
+        );
+    }
+}
+export default AddButton;
